ef793c31883ce952dcc49b12de9a3875c8a04971
mxsxs2 <mxsxs2@gmail.com>
2017-11-21 14:03:27 +0000

    Add comments

--- goaiml.go
+++ goaiml.go
@@ -2,6 +2,7 @@ package goaiml
 
 import "encoding/xml"
 
+//The name of the bot
 const BOT_NAME string = "Eliza"
 
 type AIML struct {
@@ -15,18 +16,21 @@ type AIMLRoot struct {
 	Categories []AIMLCategory `xml:"category"`
 }
 
+//Cateegory for each pattern and template
 type AIMLCategory struct {
 	XMLName  xml.Name     `xml:"category"`
 	Pattern  AIMLPattern  `xml:"pattern"`
 	Template AIMLTemplate `xml:"template"`
 }
 
+//Template structure
 type AIMLTemplate struct {
 	XMLName xml.Name `xml:"template"`
 	Content string   `xml:",innerxml"`
 	Looped  bool
 }
 
+//AIML pattern structure
 type AIMLPattern struct {
 	XMLName xml.Name `xml:"pattern"`
 	Content string   `xml:",innerxml"`
@@ -46,13 +50,15 @@ var Reflections = map[string]string{
 	"are":      "am",
 }
 
+//Function used to start a new AIML parser
 func NewAIML() *AIML {
+	//Create a new AIML
 	ret := &AIML{
 		Memory: make(map[string]string),
 		Bot:    make(map[string]string),
 	}
-
+	//Assign the bots name
 	ret.Bot["name"] = BOT_NAME
-
+	//Return the AIML parser
 	return ret
 }
